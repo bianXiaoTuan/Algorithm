82478641217ff227f5eec616e9e96c78a63a7eb6
陈欢 <chenhuan@chenhuandeMacPro.local>
2016-12-29 08:59:03 +0800

    first commit

---
+++ src/com/chenhuan0103/UnionFind/Percolation.java
@@ -0,0 +1,69 @@
+package com.chenhuan0103.UnionFind;
+
+/**
+ * Created by chenhuan on 2016/12/29.
+ */
+public class Percolation {
+    /**
+     * Create n-by-n grid, with all sites blocked
+     *
+     * @param n {int}
+     */
+    public Percolation(int n) {
+
+    }
+
+    /**
+     * Open site (row, col) if it is not open already
+     *
+     * @param row {int}
+     * @param col {int}
+     */
+    public void open(int row, int col) {
+
+    }
+
+    /**
+     * Is site (row, col) open?
+     *
+     * @param row {int}
+     * @param col {int}
+     * @return True = open, False = closed
+     */
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    /**
+     * Is site (row, col) full?
+     *
+     * @param row {int}
+     * @param col {int}
+     * @return True = full, False = not full
+     */
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    /**
+     * Number of open sites
+     *
+     * @return {int}
+     */
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    /**
+     * Does the system percolate?
+     *
+     * @return True = system percolate, False = not
+     */
+    public boolean percolates() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/chenhuan0103/UnionFind/PercolationStats.java
@@ -0,0 +1,56 @@
+package com.chenhuan0103.UnionFind;
+
+/**
+ * Created by chenhuan on 2016/12/29.
+ */
+public class PercolationStats {
+    /**
+     * Perform trials independent experiments on an n-by-n grid
+     *
+     * @param n {int} n-by-n grid
+     * @param trials {int} performs trials independent computational experiments
+     */
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    /**
+     * Sample mean of percolation threshold
+     *
+     * @return {double}
+     */
+    public double mean() {
+        return 0.1;
+    }
+
+    /**
+     * Sample standard deviation of percolation threshold
+     *
+     * @return {double}
+     */
+    public double stddev() {
+        return 0.1;
+    }
+
+    /**
+     * Low endpoint of 95% confidence interval
+     *
+     * @return {double}
+     */
+    public double confidenceLo() {
+        return 0.1;
+    }
+
+    /**
+     * High endpoint of 95% confidence interval
+     *
+     * @return {double}
+     */
+    public double confidenceHi() {
+       return 0.1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
